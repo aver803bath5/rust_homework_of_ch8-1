634c44b2fc4193300ce32d307905882adf0d7f9e
g9mein <aver803r803bath261@gmail.com>
2019-08-24 06:19:16 +0800

    feat/split-file: Split Statistics Functions Into Other Files

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
+mod statistics;
+
 fn main() {
     let mut list = vec![13, 49, 23, 19, 50, 13];
     list_vec(&list);
 
-    let mean = get_mean(&list);
-    let median = get_median(&mut list);
-    let mode = get_mode(&list);
+    let mean = statistics::get_mean(&list);
+    let median = statistics::get_median(&mut list);
+    let mode = statistics::get_mode(&list);
 
     // print!("This is a sorted list: ");
     // list_vec(&list); // This line of list has been sorted
@@ -19,56 +21,4 @@ fn list_vec(vec: &Vec<i32>) {
         print!("{}, ", val);
     }
     println!();
-}
-
-fn get_mean(list: &Vec<i32>) -> f32 {
-    let length = list.len() as f32;
-    let mut total = 0 as f32;
-    let mean: f32;
-
-    print!("Total: ");
-    for (i, val) in list.iter().enumerate() {
-        let f32_val = *val as f32;
-        let f32_i = i as f32;
-
-        if f32_i < length - 1.0 {
-            print!("{} + ", f32_val);
-        } else {
-            print!("{} =", f32_val);
-        }
-
-        total += f32_val;
-    }
-
-    println!(" = {}", total);
-
-    mean = total / length;
-    mean
-}
-
-fn get_median(list: &mut Vec<i32>) -> i32 {
-    let mid = list.len() / 2 - 1;
-    list.sort();
-    list[mid]
-}
-
-fn get_mode(list: &Vec<i32>) -> i32 {
-    use std::collections::HashMap;
-    let mut map = HashMap::new();
-
-    for val in list {
-        let count = map.entry(val).or_insert(0);
-        *count += 1;
-    }
-
-    let mut max: u32 = 0;
-    let mut _max_key = 0;
-    for (_key, val) in &map {
-        if *val > max {
-            max = *val;
-            _max_key = **_key;
-        }
-    }
-
-    _max_key
 }
